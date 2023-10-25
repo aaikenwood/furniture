a038d1544b10b8bfa35d3b171133a3252184eaa4
Archie AW <archiejaikenwood@gmail.com>
2023-10-25 14:39:18 +0100

    updated speaker exceptions

--- src/main/java/com/qa/Speaker.java
+++ src/main/java/com/qa/Speaker.java
@@ -37,27 +37,14 @@ public class Speaker extends Furniture implements ISwitchable {
 
     @Override
     public String switchOn() throws InvalidToggleException {
-        try {
-            if (isOn) {
-                throw new InvalidToggleException(String.format("You can't turn the %s on because it is already on", this.getFurnitureType()));
-            }
-        }catch (InvalidToggleException e) {
-            System.out.println(e.getMessage());
-        }
+        if (isOn) throw new InvalidToggleException(String.format("You can't turn the %s on because it is already on", this.getFurnitureType()));
         isOn = true;
         setVolume(getMaxVolume());
         return String.format("This %s has been turned off and the brightness is %.2f", this.getFurnitureType(), this.getVolume());
     }
-
     @Override
     public String switchOff() throws InvalidToggleException {
-        try {
-            if (!isOn) {
-                throw new InvalidToggleException(String.format("You can't turn the %s off because it is already off", this.getFurnitureType()));
-            }
-        }catch (InvalidToggleException e) {
-            System.out.println(e.getMessage());
-        }
+        if (!isOn) throw new InvalidToggleException(String.format("You can't turn the %s off because it is already off", this.getFurnitureType()));
         isOn = false;
         setVolume(0);
         return String.format("This %s has been turned off and the brightness is %.2f", this.getFurnitureType(), this.getVolume());
@@ -67,4 +54,5 @@ public class Speaker extends Furniture implements ISwitchable {
     public String pluggedIn() {
         return String.format("The %s %s is being charged", this.getMaterial(), this.getFurnitureType());
     }
+
 }
